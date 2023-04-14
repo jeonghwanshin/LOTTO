2b045434e08f42c521b923feaa6655daa09fa0cf
신정환 <aj7361@naver.com>
2023-04-14 14:23:04 +0900

    20230414

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,33 +21,67 @@ namespace LOTTO
     public partial class MainWindow : Window
     {
         byte[] bLotto_number = new byte[46];
-        TextBox[] aBox = new TextBox[0];
+        TextBox[] aBox = new TextBox[7];
+        Button[] aButton = new Button[46];
+        
         public MainWindow()
         {
-            box_create();
+            
             InitializeComponent();
+            create_array();
+            create_button();
         }
-        
-        private void box_create()
+        private void rand_num_create()
         {
-            aBox[0] = textBox1;
-            aBox[1] = textBox2;
-            aBox[2] = textBox3;
-            aBox[3] = textBox4;
-            aBox[4] = textBox5;
-            aBox[5] = textBox6;
+            int[] rand_num = new int[6];
+            int i = 0;
+            int j = 1;
+            Random randomobj = new Random();
+            int randbuff = 0;
+            button_clear();
+            while (i<6)
+            {
+                randbuff = randomobj.Next(1,45);
+                if (!Array.Exists(rand_num, x => x == randbuff))
+                {
+                    rand_num[i++] = randbuff;
+                    bLotto_number[randbuff] = 1;
+                    aButton[randbuff].Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE67474"));
+                }                
+            }
+            for (int k = 1; k < 7; k++)
+            {
+                aBox[k].Text = "";
+            }
+            for (int k = 1; k < 46; k++)
+            {
+                if (bLotto_number[k] > 0)
+                {
+                    aBox[j++].Text = string.Format("{0}", k);
+                }
+            }
+
         }
+        private void button_clear()
+        {
+            for(int i=1; i<46; i++)
+            {
+                aButton[i].Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffdddddd"));
+                bLotto_number[i] = 0;
+            }
+        }
+          
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
+            rand_num_create();
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
             int iCount = 0;
-            int j = 0;
+            int j = 1;
             for (int i = 0; i < 46; i++)
             {
                 if (bLotto_number[i] > 0)
@@ -69,15 +103,36 @@ namespace LOTTO
             {
                 MessageBox.Show("6개 이상 고르지 마세요.");
             }
-            
-            for(int i=0; i<46; i++)
+            for(int i=1; i<7; i++)
+            {
+                aBox[i].Text = "";
+            }
+
+            for(int i=1; i<46; i++)
             {
                 if (bLotto_number[i]>0)
                 {
-                    aBox[j++].Text(i.ToString());
+                    aBox[j++].Text = string.Format("{0}", i);
                 }
             }
 
         }
+
+        private void create_button()
+        {
+            for (int i = 1; i < 46; i++)
+            {
+                aButton[i] = this.FindName("button" + i.ToString()) as Button;
+            }
+        }
+        private void create_array()
+        {
+            for (int i = 1; i<7; i++)
+            {
+                aBox[i] = this.FindName("textBox" + i.ToString()) as TextBox;
+            }          
+        }
+
+
     }
 }
