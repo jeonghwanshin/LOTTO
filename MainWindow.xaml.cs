48451b5ad3706fd2a7a5b81139706c789d7b2494
신정환 <aj7361@naver.com>
2023-04-16 15:32:34 +0900

    2023-04-16

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
 
 namespace LOTTO
 {
@@ -33,14 +38,28 @@ namespace LOTTO
             create_array();
             create_button();
         }
-        private void rand_num_create()
+        private void rand_num_create(bool bAll_rand)
         {
             Random cRandomobj = new Random();
             int[] iaRand_num = new int[6];                        
             int iRandbuff = 0;            
             int iBoxCount = 1;
-            button_clear();
-            iCount = 0;
+            if(bAll_rand==true)
+            {
+                button_clear();
+                iCount = 0;
+            }
+            else if(iCount<6)
+            {
+                int iRandA_count_buff = 0;
+                for (int k = 1; k < 46; k++)
+                {
+                    if (baLotto_number[k] == true)
+                    {
+                        iaRand_num[iRandA_count_buff++] = k;
+                    }
+                }
+            }
             while (iCount < 6)
             {
                 iRandbuff = cRandomobj.Next(1,46);
@@ -73,9 +92,13 @@ namespace LOTTO
         }
           
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void buttonA1_Click(object sender, RoutedEventArgs e)
         {
-            rand_num_create();
+            rand_num_create(true);
+        }
+        private void buttonA2_Click(object sender, RoutedEventArgs e)
+        {
+            rand_num_create(false);
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
@@ -129,6 +152,69 @@ namespace LOTTO
             }          
         }
 
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if ( (!string.IsNullOrEmpty(textBox_num.Text)) && (int.TryParse(textBox_num.Text,out _)))
+            {
+                string strBuff = GetHttpString("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + textBox_num.Text);
+
+                if(strBuff == "")
+                {
+                    textBox_json.Text = "불러오기 실패";
+                    return;
+                } 
+                
+                textBox_json.Text = "";
+                JObject obj = JObject.Parse(strBuff);
+
+                if (obj["returnValue"].ToString() == "success")
+                {
+                    textBox_json.Text += "날짜: " + obj["drwNoDate"].ToString() + "\n";
+                    textBox_json.Text += "당첨번호: " + obj["drwtNo1"].ToString() + ", " + obj["drwtNo2"].ToString() + ", "
+                        + obj["drwtNo3"].ToString() + ", " + obj["drwtNo4"].ToString() + ", " + obj["drwtNo5"].ToString() + ", "
+                        + obj["drwtNo6"].ToString()  + "\n";               
+
+                }
+                else
+                {
+                    textBox_json.Text = "불러오기 실패"
+                }
+                
+                
+            }
+            else
+            {
+                textBox_json.Text = "제대로된 값을 입력하슈";
+            }
+            
+        }
+
+        private string GetHttpString(string strUri)
+        {
+            string strRspText = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strUri); // request 선언
+            request.Method = "GET"; // 메소드 방식 설정 /get or post
+
+            request.Timeout = 5 * 1000; // 타임아웃 설정 기본 ms 단위
+
+            using (HttpWebResponse hwr = (HttpWebResponse)request.GetResponse()) // response 메소드로 전송및 응답 요청.
+            {
+                if(hwr.StatusCode == HttpStatusCode.OK) // 정상 동작
+                {
+                    Stream respStream = hwr.GetResponseStream(); // 스트림을 생성
+                    using(StreamReader sr = new StreamReader(respStream)) // 스트림 리더로 데이터 읽을 준비 using 을 사용하는 이유는 자동으로 스트림을 닫기 위해
+                    {
+                        strRspText = sr.ReadToEnd(); // 텍스트 데이터를 얻음
+                    }
+                }
+                else
+                {
+                    strRspText = "";
+                }
+            }
+                return strRspText;
+        }
 
     }
 }
